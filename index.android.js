168b005c95d26481791df02c295c457612653485
Seth Frost <sfrost@seths-mac-mini.attlocal.net>
2017-03-22 23:58:31 -0700

    Initial commit. Hello world

---
+++ index.android.js
@@ -0,0 +1,16 @@
+/**
+ * Simple React Tutorial
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Tutorial', () => HelloWorldApp);

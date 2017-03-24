e2f6fa429a51367c1b649f492ab83f93dbaa1f1d
Seth Frost <sfrost@seths-mac-mini.attlocal.net>
2017-03-23 19:12:27 -0700

    Added a bird

--- index.android.js
+++ index.android.js
@@ -3,12 +3,18 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Image, Text, View } from 'react-native';
 
 class HelloWorldApp extends Component {
   render() {
+    let pic = {
+        uri: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Rose-Ringed_Parakeet.jpg'
+    };
     return (
-      <Text>Hello world!</Text>
+    <View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
+      <Text>Hello World!</Text>
+    </View>
     );
   }
 }

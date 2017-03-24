a7b14fe75a8cc486627f8452c06c4e01d76974bf
Seth Frost <dovekie@gmail.com>
2017-03-23 19:31:04 -0700

    Add a greeting component

--- index.android.js
+++ index.android.js
@@ -5,15 +5,25 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
+class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
+}
+
 class HelloWorldApp extends Component {
   render() {
     let pic = {
         uri: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Rose-Ringed_Parakeet.jpg'
     };
     return (
-    <View>
+    <View style={{alignItems: 'center'}}>
       <Image source={pic} style={{width: 193, height: 110}}/>
-      <Text>Hello World!</Text>
+      <Greeting name='Rose' />
+      <Greeting name='Donna' />
+      <Greeting name='Martha' />
     </View>
     );
   }
